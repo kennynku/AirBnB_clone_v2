6825b3c0d47a5c46fb652c91f0dd409ac85a087f
Ntsako Ngoveni <naction723@gmail.com>
2023-08-22 14:22:29 +0200

    test bash
    Co-authored-by: K NKUATSANA <nkuatsanakeneuoe@gmail.com>

--- test.bash
+++ test.bash
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
-# Requirements
-# pip install pycodestyle==2.7.0
-# pip install pep8==1.7.0
 
-## Code tests
-# Runs the unit tests for this project in both the
-# interactive and non-interactive modes
 echo -e "\e[104m Running Unit Tests [FileStorage] \e[0m\e[33m"
 env HBNB_MYSQL_USER="" \
     HBNB_MYSQL_HOST="" \
@@ -23,17 +17,17 @@ env HBNB_MYSQL_USER="" \
     HBNB_MYSQL_PWD="hbnb_test_pwd" \
     'python3' '-m' 'unittest' 'discover' 'tests'
 
-# echo "python3 -m unittest discover tests" | bash
+
 [ "$(echo -n $?)" == "0" ] && echo -ne "\e[100m\e[32m PASSED "
 echo -e "\e[0m"
 
-## Python code style checks
+
 echo -e "\e[104m Running Style Checks \e[0m\e[31m"
 Src_Files="$(find . -type f -regex '.*.py' | tr '\n' ' ')"
-# shellcheck disable=SC2086
+
 pycodestyle $Src_Files
 if [[ "$(($? + 0))" == "0" ]]; then
-    # shellcheck disable=SC2086
+   
     ~/.local/bin/pep8 $Src_Files
     [ "$(echo -n $?)" == "0" ] && echo -ne "\e[100m\e[32m PASSED "
 fi
