89d00da3a505cf0fc95dd272ca4e48d16a1b6ac2
K NKUATSANA <nkuatsanakeneuoe@gmail.com>
2023-08-22 14:12:22 +0200

    mysql setup updated
    Co-authored-by: Ntsako Ngoveni <naction723@gmail.com>

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,4 @@
--- Prepares the MySQL server for this project
+-- Preparation of the MySQL server
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
